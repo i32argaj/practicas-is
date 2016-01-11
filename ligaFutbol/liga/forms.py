cc2bc360175be7856ddd781dcf9da7280bb936f3
i32argaj <i32argaj@uco.es>
2016-01-11 16:16:42 +0100

    Create forms.py

---
+++ ligaFutbol/liga/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.forms import ModelForm
+from .models import *
+
+class EquipoForm(ModelForm):
+	class Meta:
+		model = Equipo
